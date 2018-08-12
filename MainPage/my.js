756378f138e33318bdf87083d11913cfc18f28f5
Cheng Yuan <cy1818cy@bupt.edu.cn>
2018-08-12 10:48:30 +0800

    update leftbar

--- MainPage/my.js
+++ MainPage/my.js
@@ -1,11 +1,13 @@
 $('#btnShowLeftBar').bind("click",btnShowLeftBar)
 function btnShowLeftBar(){
     $('#MainPage').attr("style","transform: translate(70%, 0);") //rotate3d(.25,1,.25,360deg)
+    $('#LeftBar').attr("style","transform: translate(100%, 0);")
     $('#btnShowLeftBar').unbind("click")
     $('#btnShowLeftBar').bind("click",btnHideLeftBar)
 }
 function btnHideLeftBar(){
     $('#MainPage').attr("style","transform: ;")
+    $('#LeftBar').attr("style","transform: ;")
     $('#btnShowLeftBar').unbind("click")
     $('#btnShowLeftBar').bind("click",btnShowLeftBar)
 }
