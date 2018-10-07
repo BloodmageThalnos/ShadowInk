b09ee8d1fb436934f77839ec6080d2dfd828b00b
Cheng Yuan <cy1818cy@bupt.edu.cn>
2018-10-07 17:00:46 +0800

    update .gitignore

--- appMain/ExploreAPP/admin.py
+++ appMain/ExploreAPP/admin.py
@@ -6,4 +6,4 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'author', 'pic_url', 'create_date']
     list_editable = ['title', 'author', 'pic_url']
 
-admin.site.register(models.Article,ArticleAdmin)
+admin.site.register(models.Article, ArticleAdmin)
