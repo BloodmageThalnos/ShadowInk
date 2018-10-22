b2250124e35826d680a3905e2e72bb73cc4d6eae
Cheng Yuan <cy1818cy@bupt.edu.cn>
2018-10-22 11:55:44 +0800

    update mycenter

--- appMain/MyCenterAPP/models.py
+++ appMain/MyCenterAPP/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Follower(models.Model): #关注我的用户
-    followers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers_set')
+class Follow(models.Model):
+    followers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers_set') #关注我的用户
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following_set') #我关注的用户
     build_date = models.DateTimeField(auto_now_add=True)
     followers_account = models.IntegerField(0,1000)
 
-class Follow(models.Model):   #我关注的用户
-    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following_set')
-    build_date = models.DateTimeField(auto_now_add=True)
-    following_account = models.IntegerField(0, 1000)
-
 class PersonalDetials(models.Model): #个人资料
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.FilePathField()
     phone = models.CharField(20)
+    follow = models.ManyToManyField('self',through='Follow',through_fields=('followers','following'),symmetrical=False)
--- appMain/MyCenterAPP/views.py
+++ appMain/MyCenterAPP/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def getUserInfo(user):
+    if not user.is_authenticated:
+        return {}
+    
