c9a4028b77c5c9edcbf32863f0b7ac548e3e6d9f
lang22 <34049180+lang22@users.noreply.github.com>
2018-10-24 20:27:47 +0800

    why
    wgyhwy

--- appMain/MyCenterAPP/admin.py
+++ appMain/MyCenterAPP/admin.py
@@ -3,6 +3,7 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Follow)
+admin.site.register(models.Follower)
 from django.contrib import admin
 
 # Register your models here.
